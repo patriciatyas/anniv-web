bafb0690bcb59a104756f1579fb2f68cc90e7020
patriciatyas <patriciatyas11@gmail.com>
2025-01-06 01:29:04 +0700

    fix relative

--- src/Fifth.jsx
+++ src/Fifth.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Fifth = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-dots2">
+        <div className="min-h-screen flex flex-col items-center justify-center bg-dots2 relative">
             <img src="./images/fifth-paper.png" alt="paper" className="pt-4 max-w-96 animate-float"/>
         </div>
     )

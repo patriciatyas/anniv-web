a668f8640fe0156ba83378fc83be658fc1e0693d
patriciatyas <patriciatyas11@gmail.com>
2025-01-06 00:56:35 +0700

    fix max width fifth page

--- src/Fifth.jsx
+++ src/Fifth.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Fifth = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-dots2">
-            <img src="./images/fifth-paper.png" alt="paper" className="pt-4 animate-float"/>
+            <img src="./images/fifth-paper.png" alt="paper" className="pt-4 max-w-96 animate-float"/>
         </div>
     )
 }

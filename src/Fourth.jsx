f79898c9c8e68f35811b3b6db4aeb8c24e6d3157
patriciatyas <patriciatyas11@gmail.com>
2025-01-06 00:44:17 +0700

    add quotes on fourth page and animation

--- src/Fourth.jsx
+++ src/Fourth.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Fourth = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-dots2">
-            <img src="./images/paper-1.png" alt="paper1" className="absolute"/>
-            <img src="./images/paper-2.png" alt="paper2" className="absolute"/>
-            <img src="./images/pen.png" alt="pen" className="absolute"/>
-            <img src="./images/fourth-stars.png" alt="stars" className="absolute"/>
+        <div className="min-h-screen flex flex-col items-center justify-center bg-dots2 relative">
+            <img src="./images/paper-1.png" alt="paper1" className="absolute top-40 left-4 animate-float w-72"/>
+            <img src="./images/paper-2.png" alt="paper2" className="absolute right-4 bottom-28 animate-float w-72"/>
+            <img src="./images/pen.png" alt="pen" className="absolute top-60 right-20 w-14"/>
+            <img src="./images/fourth-stars.png" alt="stars" className="absolute animate-pulse w-80"/>
         </div>
     )
 }

66d35c1ee75eac0fdeb57bd7edb19a687a57e3fb
patriciatyas <patriciatyas11@gmail.com>
2025-01-06 00:47:09 +0700

    add paragraph for fifth page

--- src/Fifth.jsx
+++ src/Fifth.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const Fifth = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-dots2">
-            <img src="./images/fifth-paper.png" alt="paper" className="w-96 pr-8"/>
-            <img src="./images/flowers.png" alt="flowers" className="absolute bottom-32"/>
+            <img src="./images/fifth-paper.png" alt="paper" className="pt-4 animate-float"/>
         </div>
     )
 }

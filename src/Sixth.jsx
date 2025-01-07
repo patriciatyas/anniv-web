1cc38e424a50c861831e77f0e9d835d2d26a3a84
patriciatyas <patriciatyas11@gmail.com>
2025-01-07 12:48:46 +0700

    change video size

--- src/Sixth.jsx
+++ src/Sixth.jsx
@@ -4,7 +4,7 @@ const Sixth = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-dots2 relative">
             <div className="flex items-center justify-center w-full h-full">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/wr3LNYTa1MM?si=9Je5Y0tBOm3GtXAT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="660" height="415" src="https://www.youtube.com/embed/wr3LNYTa1MM?si=9Je5Y0tBOm3GtXAT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     )

5306424c58b1240a217a5c311916fc1a4e9ae2df
patriciatyas <patriciatyas11@gmail.com>
2025-01-07 12:46:45 +0700

    change video

--- src/Sixth.jsx
+++ src/Sixth.jsx
@@ -3,13 +3,8 @@ import React from "react";
 const Sixth = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-dots2 relative">
-            <div className="flex items-center justify-center">
-                <video
-                    className="w-full"
-                    controls
-                >
-                    <source src="video-pres.mp4" type="video/mp4" />
-                </video>
+            <div className="flex items-center justify-center w-full h-full">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/wr3LNYTa1MM?si=9Je5Y0tBOm3GtXAT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     )

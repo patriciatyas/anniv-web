cd86dc72526dae8eb29b1af4b29d7c6678888838
patriciatyas <patriciatyas11@gmail.com>
2025-01-07 16:16:09 +0700

    easter egg

--- src/Tyas.jsx
+++ src/Tyas.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Tyas = () => {
     return (
+        <div className='flex flex-col items-center justify-center min-h-screen'>
         <h1>halo sy yg buat webnya ahahahahahahah</h1>
+        <div className='w-28'>
+            <img src="./images/rickroll.gif" alt="rickroll"/>
+        </div>
+        </div>
     )
 }
 
